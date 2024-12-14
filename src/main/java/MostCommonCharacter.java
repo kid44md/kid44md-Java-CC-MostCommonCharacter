d77d9f84654517c5a3e462476c7a62ef0f8d06bc
Matt Fitzgerald <kid44md@gmail.com>
2024-12-14 02:29:34 +0000

    finished mostcommon

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,47 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+
+//FIRST CASE 
+//Within these numbers, 1 appears the most. X
+//SECOND CASE
+//Within these numbers, 4 appears the most. X
+//THIRD CASE
+//With these numbers, 7 appears the most. X
+//FOURTH CASE
+//Within these numbers, 6 appears the most. X
+//FIFTH CASE
+//Within these numbers, 5 appears the most. X
+//SIXTH CASE
+//Within this string, n appears the most. X
+
+    HashMap<Character, Integer> map = new HashMap<>();
+    int maxValue = 0;
+    char mostCommon = ' ';
+for( int i = 0; i < str.length(); i++){
+    if(!map.containsKey(str.charAt(i))){
+        map.put(str.charAt(i), 1);
+    }else{
+        map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+    }
+}
+
+
+
+
+       
+// map.forEach(
+//     (key, value)
+//         -> System.out.println(key + " = " + value));
+
+        for (char key : map.keySet()) {
+            if(map.get(key) > maxValue){
+                maxValue = map.get(key);
+                mostCommon = key;
+            }
+            System.out.println("Key: " + key + ", Value: " + map.get(key));
+        }
+System.out.println(mostCommon + " RESULTS");
+        return mostCommon;
     }
 }
